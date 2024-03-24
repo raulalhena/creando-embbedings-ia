b3273648721e083008f786ca8e00d662e79e4e4f
Raul <raul.alhena@gmail.com>
2024-03-24 18:35:47 +0100

    huggingface & "local" & browser embbedings creator and compare similarity working

---
+++ apps/embedding-browser/main.js
@@ -0,0 +1,46 @@
+import { pipeline, env } from "https://cdn.jsdelivr.net/npm/@xenova/transformers@2.6.0";
+
+env.allowLocalModels = false;
+
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const generateButton = document.getElementById('generate-button');
+const output = document.getElementById('output');
+
+const generateEmbeddings = await pipeline(
+  'feature-extraction',
+  'Xenova/all-MiniLM-L6-v2'
+);
+
+generateButton.disabled = false;
+
+function dotProduct(a, b) {
+  try{
+    if(a.length !== b.length) throw new Error("Both arguments must have the same length");
+    let result = 0;
+
+    for(let i = 0; i < a.length; i++) {
+      result += a[i] * b[i];
+    }
+
+    return result;
+  }catch(error) {
+    console.log(error);
+  }
+}
+
+generateButton.addEventListener('click', async () => {
+  const output1 = await generateEmbeddings(input1.innerText, {
+    pooling: 'mean',
+    normalize: true,
+  });
+
+  const output2 = await generateEmbeddings(input2.innerText, {
+    pooling: 'mean',
+    normalize: true,
+  });
+
+  const similarity = dotProduct(output1.data, output2.data)
+  
+  output.innerText = similarity;
+})
